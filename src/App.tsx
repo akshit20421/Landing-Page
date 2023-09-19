4825063f2ea4bf2e30c378066fdc5ef25e63318d
Akshit <103023426+akshit20421@users.noreply.github.com>
2023-09-19 12:31:18 +0530

    Folders Update

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Navbar from './components/navbar/Navbar'
+const App = () => {
+  return (
+   <Navbar/>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <h1 className="head">Hello</h1>
+  )
+}
+
+export default Navbar
