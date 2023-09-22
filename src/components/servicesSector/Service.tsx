47011b018abed8e51829da09b6d244d3ed0b01e8
Akshit <103023426+akshit20421@users.noreply.github.com>
2023-09-22 21:42:12 +0530

    Updated Service Section

--- src/components/servicesSector/Service.tsx
+++ src/components/servicesSector/Service.tsx
@@ -2,7 +2,37 @@ import styles from "./serviceStyle.module.css"
 
 const Service = () => {
   return (
-    <div className={styles.serviceSector}>Services</div>
+    <div className={styles.serviceSector}>
+<h1>Destiny isn't A.I, its the result of human intelligence.</h1>
+        <div className={styles.paraService}>  
+        <p>Ne homero petentium mel, eu pro putent persecuti. Id ius mutat gubergren, eros harum hendrerit ex eos, in quo vocibus inimicus gubergren.</p>
+        </div>  
+        <div className={styles.serviceFeature}>
+            <div className={styles.serviceF1}>
+                <div className={styles.borderImgServices}>
+                    <img src="src\assets\icon1.png" alt="" />
+               </div>
+               <h2>Highly Responsive</h2>
+                <p>Id ius mutat gubergren, eros harum hendrerit ex eos, in quo vocibus inimicus gubergren. Experience, then believe.</p>
+            </div>
+            <div className={styles.serviceF2}>
+            <div className={styles.borderImgServices}>
+                <img src="src\assets\icon2.png" alt="" />
+               </div>
+               <h2>Built-in Security</h2>
+               <p>Id ius mutat gubergren, eros harum hendrerit ex eos, in quo vocibus inimicus gubergren. Experience, then believe.</p>
+                
+            </div>
+            <div className={styles.serviceF3}>
+            <div className={styles.borderImgServices}>
+                <img src="src\assets\icon3.png" alt="" />
+               </div>
+               <h2>Safety Locked</h2>
+               <p>Id ius mutat gubergren, eros harum hendrerit ex eos, in quo vocibus inimicus gubergren. Experience, then believe.</p>
+            </div>
+            
+        </div>
+    </div>
   )
 }
 
