1b343f91e8ef8c8f0fab177e12f3ee7b7ba4d426
Akshit <103023426+akshit20421@users.noreply.github.com>
2023-09-23 15:43:06 +0530

    Features Section Completed!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/heroSection/Hero'
 import "./styles.css"
 import Service from "./components/servicesSector/Service"
+import Feature from './components/FeatureSection/Feature'
 const App = () => {
   return (
     <>
    <Hero/>
-   <Service/></>
+   <Service/>
+   <Feature/></>
   )
 }
 
---
+++ src/components/FeatureSection/Feature.tsx
@@ -0,0 +1,65 @@
+
+import styles from "./feature.module.css"
+const Feature = () => {
+  return (
+    <div className={styles.featureMain}>
+        <div className={styles.feature1}>
+         <div className={styles.imgFeature1}>
+             <img src="src\assets\feature_1.png" alt="" />
+         </div>
+         <div className={styles.dataFeature1}>
+            <h1>Designed for Startups & brands.</h1>
+            <p>Just get the code and sit tight, you will witness its power and performance in lead generations. Is simple yet Powerful and productive technology. Experience, then believe.</p>
+            <div className={styles.bulletFeature1}>
+                <div className={styles.bulletFeature11}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Write some feature here.</p>
+                </div>
+                <div className={styles.bulletFeature22}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Repeat the same with another one.</p>
+                </div>
+                <div className={styles.bulletFeature33}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Complete the list with the last.</p>
+                </div>
+            </div>
+            <div className={styles.btnFeatureSection}>
+                <button>Watch Intro</button>
+            </div>
+            </div>
+        </div>
+        <div className={styles.feature2}>
+      
+         <div className={styles.imgFeature1}>
+             <img src="src\assets\feature_1.png" alt="" />
+         </div>
+         <div className={styles.dataFeature2}>
+            <h1>Designed for Startups & brands.</h1>
+            <p>Just get the code and sit tight, you will witness its power and performance in lead generations. Is simple yet Powerful and productive technology. Experience, then believe.</p>
+            <div className={styles.bulletFeature2}>
+                <div className={styles.bulletFeature111}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Write some feature here.</p>
+                </div>
+                <div className={styles.bulletFeature222}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Repeat the same with another one.</p>
+                </div>
+                <div className={styles.bulletFeature333}>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></g></svg>
+         <p>Complete the list with the last.</p>
+                </div>
+            </div>
+            <div className={styles.btnFeatureSection}>
+                <button>Watch Intro</button>
+            </div>
+            </div>
+        
+            </div>
+            
+    </div>
+  )
+}
+
+export default Feature
